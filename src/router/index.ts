cc5d71c71401cc1f095261f5af07636d88fb01a1
ZlxmChen <zlxmmail@126.com>
2024-05-28 22:45:35 +0800

    添加路由守卫，添加logout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from './routes';
 
-export default createRouter({
+const router = createRouter({
   routes,
   history: createWebHashHistory(),
   scrollBehavior() {
     return { top: 0 };
   },
 });
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach', localStorage.getItem('token'));
+  if (to.path !== '/login' && localStorage.getItem('token') == '') {
+    // 如果用户未登录且目标路由不是登录页，重定向到登录页
+    next('/login');
+  } else {
+    // 否则，放行
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/settings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="settings-container">
+    <n-button type="warning" @click="logout">退出登录</n-button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const logout = () => {
+  localStorage.setItem('token', '');
+  router.push('login');
+};
+</script>
+
+<style lang="scss" scoped>
+.settings-container {
+  padding: 20px;
+}
+</style>
