1a9e6a0c27c9b80499f13262920b6f7aabe15ed4
ZlxmChen <zlxmmail@126.com>
2024-06-11 16:14:56 +0800

    nin

--- src/components/importAI.vue
+++ src/components/importAI.vue
@@ -547,7 +547,7 @@ const generatePoster = () => {
   }
   let randomInt = Math.floor(Math.random() * 200) + 100;
   intervalId = setInterval(() => {
-    genView.percent += 4;
+    genView.percent += Math.floor(Math.random() * 2) + 1;
     if (genView.percent >= 100) {
       genView.color = '#5cb85c';
       clearInterval(intervalId);
